698d6c4de9b6adfa164912f3760c938064a680cb
urmom999999 <alvarodelvalled@gamil.com>
2025-10-27 20:11:12 +0100

    mklm

--- src/Cliente.java
+++ src/Cliente.java
@@ -34,7 +34,7 @@ public static void siServido(){
 @Override
 public void run(){
     //ENTRAR, ANUNCIAR ENTRADA -----HORA-----
-    System.out.println(nombre+ " entró en la cafetería y pidió un café. A la hora "+ llegada+ "maximo de espera:"+ tiempoEspera);
+    System.out.println(nombre+ " entró en la cafetería y pidió un café. Máximo de espera: "+ tiempoEspera);
 
 
     //ESPERAR DETERMINADO TIEMPO
